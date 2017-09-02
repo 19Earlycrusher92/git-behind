27c89b2425e2740c2121e6f9ddfb1a3f4290f17f
Christer Eckermann <ecker00@gmail.com>
2017-09-02 23:19:49 +0200

    Removed default template files

--- spec/git-behind-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import GitBehindView from '../lib/git-behind-view';
-
-describe('GitBehindView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
