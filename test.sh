f79765d75833d39768cffc8d8cd622d6340a8da8
Christer Eckermann <ecker00@gmail.com>
2017-09-02 23:45:56 +0200

    Removed unused files

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-branch="origin/master"
-update=$(git fetch -v --dry-run 2>&1 | grep "$branch" | grep -v "up to date")
-if [ "$update" = "" ];
-	then echo "Already up to date";
-	else  echo "You are behind $branch";
-fi
